0416f9b6c2f6948374b022bf0bb209699d889b75
Sipho-cmd <ST10457072@rcconnect.edu.za>
2026-04-10 13:49:32 +0200

    Added validation and loop

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //Registration
+        // Registration
         Registration registration = new Registration();
         registration.Register();
 
-
-        //login attempt
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Please login in: ");
-        System.out.println("Enter Username: ");
-        String loginUser = s.nextLine();
-
-        System.out.println("Enter Password: ");
-        String loginPass = s.nextLine();
-
-        //Creates a login object
         Login login = new Login(
                 registration.userName,
                 registration.passWord,
                 registration.firstName,
                 registration.lastName);
 
-        System.out.println(login.loginUser(loginUser, loginPass));
+        boolean success = false;
+
+        while (!success) {
+            System.out.println("Please login in: ");
+
+            System.out.println("Enter Username: ");
+            String loginUser = s.nextLine();
+
+            System.out.println("Enter Password: ");
+            String loginPass = s.nextLine();
+
+            String result = login.loginUser(loginUser, loginPass);
+            System.out.println(result);
+
+            if (result.equals("Login successful")) {
+                success = true;
+            } else {
+                System.out.println("Try again...\n");
+            }
+        }
     }
 }
